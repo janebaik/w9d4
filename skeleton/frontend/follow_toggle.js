a1028d6a9032a945a10a5fe17db80f30c41f2ddf
Paul Stonebraker <wpstonebraker@gmail.com>
2020-12-31 16:50:26 -0500

    work on handle click

--- skeleton/frontend/follow_toggle.js
+++ skeleton/frontend/follow_toggle.js
@@ -6,16 +6,46 @@ class FollowToggle {
     this.$el = $el;
 
     this.render();
+    this.handleClick();
   }
 
-  render(){
-    if (this.followState === "unfollowed"){
+  render() {
+    if (this.followState === "unfollowed") {
       this.$el.text("follow");
     } else if (this.followState === "followed") {
       this.$el.text("unfollow");
     }
   }
+
+  followUser(id) {
+    return $.ajax({
+      method: "POST",
+      url: `/users/${id}/follow`,
+    });
+  }
+
+  handleClick() {
+    this.$el.on("click", (e) => {
+      // debugger;
+      e.preventDefault();
+      // debugger;
+      alert("line 23");
+      this.followUser(this.userId);
+      // debugger;
+    });
+
+    // this.$el.click(function (event) {
+    //   event.preventDefault();
+    //   alert("line 23");
+    //   // if (this.followState === "unfollowed") {
+    //   debugger;
+    //   return $.ajax({
+    //     method: "POST",
+    //     url: "/users/:id/follow",
+    //   });
+    //   // }
+    // });
+  }
 }
 
 module.exports = FollowToggle;
-
